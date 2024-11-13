9c6d7f722a328666e4413a42b6556b4101d6335f
Quyet's Code <quyetcoccoc@gmail.com>
2024-11-13 13:53:41 +0700

    backend: album controller

--- backend/src/controller/album.controller.js
+++ backend/src/controller/album.controller.js
@@ -1,8 +1,35 @@
 import { Album } from "../models/album.model";
 import { Song } from "../models/song.model";
 
-const getAllAlbums = async (req, res) => {};
+const getAllAlbums = async (req, res) => {
+  try {
+    const albums = await Album.find();
 
-const getAlbumsById = async (req, res, next) => {};
+    if (!albums) {
+      console.error("There is no album");
+    }
+
+    res.status(200).json(albums);
+  } catch (error) {
+    console.log("Error fetching all album", error);
+    next(error);
+  }
+};
+
+const getAlbumsById = async (req, res, next) => {
+  try {
+    const { alBumId } = req.params;
+
+    const album = await Album.findById(alBumId).populate("songs");
+
+    if (!album) {
+      return res.status(404).json({ message: "Album not found" });
+    }
+
+    res.status(200).json(album);
+  } catch (error) {
+    next(error);
+  }
+};
 
 export { getAllAlbums, getAlbumsById };
